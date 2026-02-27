4b4243dbb66c060024c14392acbb9a5d8224f766
arkabarua69 <arkaturna@gmail.com>
2026-02-27 13:18:44 +0600

    Add Vercel Speed Insights for performance monitoring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { CartProvider } from "@/contexts/CartContext";
 import { WishlistProvider } from "@/contexts/WishlistContext";
@@ -94,6 +95,7 @@ const App = () => {
           </BrowserRouter>
           <ChatBot />
           <InstallPrompt />
+          <SpeedInsights />
         </TooltipProvider>
       </WishlistProvider>
       </CartProvider>
